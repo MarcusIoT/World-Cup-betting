0aad6d1a32576e8ccd00d6b742f9b0cef416b2b2
Marcus Schoch <magge.schoch@gmail.com>
2018-06-22 13:31:12 +0200

    added comments to IO

--- IO.java
+++ IO.java
@@ -3,21 +3,25 @@ import java.lang.*;
 import java.nio.file.*;
 
 /**
- * Write a description of class Auslesen here.
+ * Diese Klasse übernimmt das Auslesen und Speichern von Informationen in .txt Dateien.
  *
  * @author Marcus Schoch
- * @version 17.06.2018
+ * @version 22.06.2018
  */
 public class IO
 {
     /**
-     * Constructor for objects of class Auslesen
+     * Kostruktor für Objekte der Klasse IO
      */
     public IO()
     {
 
     }
 
+    /**
+     * Liest alle Zeilen aus einer Textdatei mit dem übergebenen Namen und Ordnerverzeichnis aus und übergibt diese als String.
+     * Die Zeilen und damit auch die einzelnen Informationen sind in dem String durch ein "/" getrennt.
+     */
     public String ladeDatei(String ordner, String dateiname) throws IOException
     {
         String datei = ordner + "/" + dateiname + ".txt";
@@ -25,7 +29,6 @@ public class IO
         BufferedReader br = new BufferedReader(fr);
 
         String daten = "";
-
         String zeile = "";
 
         while( (zeile = br.readLine()) != null )
@@ -40,7 +43,8 @@ public class IO
     }
 
     /**
-     * 
+     * Speichert übergebenes Array in die Text Datei des Landes, dessen Namen in dem Array mit übergeben wird.
+     * Jeder Wert des Arrays wird in einer neuen Zeile gespeichert.
      */
     public  void speichereLand(String[] teile) throws IOException
     {
@@ -48,24 +52,23 @@ public class IO
         FileWriter fw = new FileWriter(datei);
         BufferedWriter bw = new BufferedWriter(fw);
 
-
         for (int x = 0; x < teile.length; x++) {
             bw.write(teile[x]);
-            if(x<teile.length){bw.newLine();};
+            if(x<teile.length-1){bw.newLine();};
         }
 
         bw.close();
     }
-    
+
     /**
-     * 
+     * Speichert übergebenes Array in die Text Datei der Gruppe, dessen Namen in dem Array mit übergeben wird.
+     * Jeder Wert des Arrays wird in einer neuen Zeile gespeichert.
      */
     public  void resetGruppe(String[] teile) throws IOException
     {
         String datei = "Gruppen/" + teile[0] + ".txt";
         FileWriter fw = new FileWriter(datei);
         BufferedWriter bw = new BufferedWriter(fw);
-
 
         for (int x = 1; x < teile.length; x++) {
             bw.write(teile[x]);
@@ -74,16 +77,16 @@ public class IO
 
         bw.close();
     }
-    
+
     /**
-     * 
+     * Speichert übergebenes Array in die Text Datei der Gruppe, dessen Namen separat übergeben wird.
+     * Jeder Wert des Arrays wird in einer neuen Zeile gespeichert.
      */
     public  void speichereGruppe(String name, String[] teile) throws IOException
     {
         String datei = "Gruppen/" + name + ".txt";
         FileWriter fw = new FileWriter(datei);
         BufferedWriter bw = new BufferedWriter(fw);
-
 
         for (int x = 0; x < teile.length; x++) {
             bw.write(teile[x]);
@@ -94,7 +97,7 @@ public class IO
     }
 
     /**
-     * 
+     * Fügt übergebenen String lediglich der text Datei "Gruppen" hinzu, die vorhandenen Einträge bleiben dabei bestehen.
      */
     public  void appendGruppe(String daten) throws IOException
     {
@@ -105,12 +108,11 @@ public class IO
         bw.newLine();
         bw.write(daten);
 
-        
         bw.close();
     }
-    
+
     /**
-     * 
+     * Löscht die Text Datei mit dem übergebenen Namen in dem übergebenen Verzeichnis.
      */
     public  void löscheDatei(String ordner, String name) throws IOException
     {
